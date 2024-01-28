235f6d3c0a3c8dcc4bced7b78c6cb04fa5e47972
Luca Puglia <luca.puglia89@gmail.com>
2024-01-28 23:49:18 +0100

    removed files

--- itatv/server.js
+++ itatv/server.js
@@ -4,6 +4,4 @@ const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
 const addonInterface = require("./addon")
 serveHTTP(addonInterface, { port: process.env.PORT || 80 })
 
-// when you've deployed your addon, un-comment this line
 publishToCentral("https://prova-sr3g.onrender.com/manifest.json")
-// for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
