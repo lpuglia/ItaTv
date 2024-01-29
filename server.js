f6d5b7080ba2e860236032902b24653aaa8f15c0
Luca Puglia <luca.puglia89@gmail.com>
2024-01-29 09:53:46 +0100

    .

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
 const addonInterface = require("./addon")
 serveHTTP(addonInterface, { port: process.env.PORT || 80 })
 
-publishToCentral("https://6ef53e8aac88-itatv.baby-beamup.club/manifest.json")
+// publishToCentral("https://6ef53e8aac88-itatv.baby-beamup.club/manifest.json")
