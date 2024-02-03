0aae761c1fbd79b54c3408b276ac3bf47ced7dcf
Luca Puglia <luca.puglia89@gmail.com>
2024-02-03 20:02:20 +0100

    log uri

--- mongodictionary.js
+++ mongodictionary.js
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 class MongoDictionary {
     constructor(collectionName, verbose=true) {
         const uri = `mongodb+srv://${process.env.USR}:${process.env.PWD}@${process.env.SRVR}/${process.env.DB}?retryWrites=true&w=majority`;
-
+        console.log(uri)
         this.client = new MongoClient(uri, {
             serverApi: {
             version: ServerApiVersion.v1,
