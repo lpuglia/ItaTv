af68da7e17432d5eed0431276009ca6829a14c9a
Luca Puglia <luca.puglia89@gmail.com>
2024-06-17 22:07:00 +0200

    bump

--- addon.js
+++ addon.js
@@ -18,7 +18,7 @@ function getPublicIpSync() {
 
 var manifest = {
     "id": "it.itatv",
-    "version": "0.3.1",
+    "version": "0.3.2",
     "logo": "https://i.imgur.com/UFmjxIQ.png",
     "background": "https://i.imgur.com/zoEMlhv.png",
 
